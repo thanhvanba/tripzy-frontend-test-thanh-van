02c5269e9c6ed43faa25e982d4e237dfe60e87b7
thanhvanba <vanbatrungthanh@gmail.com>
2025-11-18 14:36:13 +0700

    feat: add search page and integrate Nunito Sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Nunito_Sans } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-nunito",
+});
 
 export const metadata: Metadata = {
   title: "Tripzy - Travel Smarter",
@@ -13,9 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
-        <AntdRegistry>{children}</AntdRegistry>
+    <html lang="en" className={nunito.className}>
+      <body className="antialiased">
+        <AntdRegistry>
+          <ConfigProvider
+            theme={{
+              token: {
+                fontFamily: "var(--font-nunito)",
+              },
+            }}
+          >
+            {children}
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import Header from "../components/header";
+
 export default function SearchPage() {
-  return <div>Search Page</div>;
+  const searchParams = useSearchParams();
+
+  const from = searchParams.get("from") || "-";
+  const to = searchParams.get("to") || "-";
+  const departure = searchParams.get("dep") || "-";
+  const returnDate = searchParams.get("ret") || null;
+  const passengers = searchParams.get("pax") || "1";
+
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <div className="relative">
+        <div className="flex flex-col items-center justify-center h-[45vh] py-12 pt-24 bg-linear-to-b from-[#F5F8FF] to-[#DBF5FF] relative"></div>
+
+        <div className="container absolute top-28 left-1/2 -translate-x-1/2">
+          <div className="flex flex-col items-center justify-center w-full">
+            <div className="w-full min-w-2xl bg-white rounded-2xl shadow-lg px-24 py-20">
+              <div className="space-y-14 mb-8 text-[#121216] text-2xl font-semibold">
+                <div className="">From: {from}</div>
+                <div className="">To: {to}</div>
+                <div className="">Departure date: {departure}</div>
+                {returnDate && (
+                  <div className="">Return date: {returnDate}</div>
+                )}
+                <div className="">No. of passenger {passengers}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
